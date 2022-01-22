b289bbc81ee0a4c70fd9be9ee263b1ab7f6be4f2
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-01-22 23:30:57 +0100

    debian

--- pkg/modules/packages.go
+++ pkg/modules/packages.go
@@ -22,13 +22,48 @@ func (p *Packages) PaseConfig() bool {
 }
 
 func (p Packages) Run(dryRun bool) string {
-	for _, pkg := range p.packages {
-		fmt.Printf("Install package %s\n", pkg)
-		out, err := exec.Command("sudo", "dnf", "-y", "install", pkg).CombinedOutput()
-		if err != nil {
-			panic(err)
+	osReleaseFile, err := os.ReadFile("/etc/os-release")
+	if err != nil {
+		panic(err)
+	}
+	osReleaseString := string(osReleaseFile)
+	rhLike := false
+	debianLike := false
+	for _, v := range strings.Split(osReleaseString, "\n") {
+		entry := strings.Split(v, "=")
+		if entry[0] == "ID" {
+			if entry[1] == "debian" || entry[1] == "ubuntu" {
+				fmt.Println("OS is debian-like")
+				debianLike = true
+			}
+
+			if entry[1] == "fedora" || entry[1] == "centos" {
+				fmt.Println("OS is rh-like")
+				rhLike = true
+			}
 		}
-		fmt.Println(string(out))
+	}
+
+	if debianLike {
+		for _, pkg := range p.packages {
+			fmt.Printf("Install package %s\n", pkg)
+			out, err := exec.Command("sudo", "apt-get", "-y", "install", pkg).CombinedOutput()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(out))
+		}
+	}
+
+	if rhLike {
+		for _, pkg := range p.packages {
+			fmt.Printf("Install package %s\n", pkg)
+			out, err := exec.Command("sudo", "dnf", "-y", "install", pkg).CombinedOutput()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(out))
+		}
 	}
 
 	return "xx"
